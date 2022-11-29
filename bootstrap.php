51baa7c03ba4a7c318b9606127eb2ea02e822dc0
grogg19 <differ@list.ru>
2022-11-29 23:03:49 +0300

    init

---
+++ bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+
+define('APP_DIR',  $_SERVER['DOCUMENT_ROOT']); // Корень проекта
+define('VIEW_DIR',  $_SERVER['DOCUMENT_ROOT'] . '/layout/'); // Корень всех представлений
+define('SITE_ROOT',  'http://' . $_SERVER['SERVER_NAME']); // Корень проекта
+
+date_default_timezone_set('Europe/Moscow');
---
+++ src/Tasks/TaskSecond.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Tasks;
+
+class TaskSecond
+{
+
+}
+l
