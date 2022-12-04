4e9bafcc4f38010314eb623509f89835197c45ec
grogg19 <differ@list.ru>
2022-12-05 01:59:59 +0300

    fix(config): изменил имена констант и .example.env

--- config/database.php
+++ config/database.php
@@ -1,11 +1,13 @@
 <?php
+
 /**
  * Параметры подключения к БД
  */
+
 return [
-    'driver' => 'mysql',
-    'host' => env('HOST_NAME', 'localhost'),
-    'username' => env('MYSQL_USER', 'db'),
-    'password' => env('MYSQL_PASSWORD', 'password'),
-    'database' => env('MYSQL_DATABASE', 'db')
+    'driver'   => 'mysql',
+    'host'     => 'localhost_db',
+    'username' => env('DB_USERNAME', 'db'),
+    'password' => env('DB_PASSWORD', 'password'),
+    'database' => env('DB_DATABASE', 'db'),
 ];
