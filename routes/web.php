cfaeffaed4b5c14132a793a7a208c8acf60666e9
grogg19 <differ@list.ru>
2022-12-05 03:14:17 +0300

    feat(controller): Добавлен контроллер для второго задания

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Controllers\AdditionBigNumbersController;
 use App\Controllers\CustomerController;
 use App\Exception\NotFoundException;
 use App\Migrations\MigrationLoader;
@@ -8,13 +9,15 @@ use App\Router;
 $router = new Router(); // создаем маршрутизатор
 
 /** Запуск миграций  */
-$router->get('get','/installdb', MigrationLoader::class . '@makeMigrations');
+$router->get('get', '/installdb', MigrationLoader::class . '@makeMigrations');
 
-/** Главная страница */
-$router->get('get','/', CustomerController::class . '@index');
+/** Задача №1 */
+$router->get('get', '/', CustomerController::class . '@index');
 
+/** Задача №2 */
+$router->get('get', '/second-task', AdditionBigNumbersController::class . '@index');
 
 /** Страница 404  */
-$router->get('get','/404', NotFoundException::class . '@render');
+$router->get('get', '/404', NotFoundException::class . '@render');
 
 return $router;
