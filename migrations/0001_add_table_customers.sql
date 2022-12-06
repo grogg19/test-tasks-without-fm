5a35a94f04533d07fa9a7c7f6eab92f8dcfcb720
grogg19 <differ@list.ru>
2022-12-06 04:34:35 +0300

    fix(migration): сменил название у столбца gender

--- migrations/0001_add_table_customers.sql
+++ migrations/0001_add_table_customers.sql
@@ -2,9 +2,9 @@ CREATE TABLE IF NOT EXISTS `customers` (
     `id` INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     `first_name` VARCHAR(255) NULL,
     `last_name` VARCHAR(255) NULL,
-    `gender` tinyint(1) NOT NULL DEFAULT 0 COMMENT '0 – пол не указан, 1 - юноша, 2 - девушка.',
+    `gender_id` tinyint(1) NOT NULL DEFAULT 0 COMMENT '0 – пол не указан, 1 - юноша, 2 - девушка.',
     `birth_date` timestamp NULL COMMENT 'День рождения в Unix Time Stamp.'
-    )
+)
     ENGINE = innodb
     AUTO_INCREMENT = 1
     CHARACTER SET utf8
