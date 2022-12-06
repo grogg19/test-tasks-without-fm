70ec734ca6084cb80f3abd398640fd609a8f688b
grogg19 <differ@list.ru>
2022-12-06 04:35:04 +0300

    fix(migration): second_name теперь может быть null

--- migrations/0002_add_table_authors.sql
+++ migrations/0002_add_table_authors.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS `authors` (
     `id` INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     `last_name` VARCHAR(255) NOT NULL,
     `first_name` VARCHAR(255) NOT NULL,
-    `second_name` VARCHAR(255) NOT NULL
+    `second_name` VARCHAR(255) NULL
 )
     ENGINE = innodb
     AUTO_INCREMENT = 1
