6d8619465c5d681c28a3c33377aa63451112fd89
grogg19 <differ@list.ru>
2022-12-06 04:36:03 +0300

    fix(migration): нормализация таблицы books

--- migrations/0003_add_table_books.sql
+++ migrations/0003_add_table_books.sql
@@ -1,9 +1,10 @@
 CREATE TABLE IF NOT EXISTS `books` (
     `id` INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    `customer_id` INT(11) NOT NULL,
-    `author_id`  INT(11) NOT NULL,
-    `book_title` VARCHAR(255) DEFAULT NULL
-    )
+    `author_id` INT(11) UNSIGNED NOT NULL,
+    `book_title` VARCHAR(255) DEFAULT NULL,
+    CONSTRAINT books_authors_fk
+        FOREIGN KEY (author_id) REFERENCES authors(id)
+)
     ENGINE = innodb
     AUTO_INCREMENT = 1
     CHARACTER SET utf8
